379c3d2a926c71d79cc63f929d6bec9dc2b97151
Fabio Bier <fabio@proxymail.nl>
2018-02-28 00:11:48 +0100

    Turned the main App container into a pure-component since there is no need for a seperate state besides the Redux store.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AddMarker from './containers/addMarker';
 import VisibleMarkerList from './containers/visibleMarkerList';
 import Footer from './components/footer';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <AddMarker />
-        <VisibleMarkerList />
-        <Footer />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <AddMarker />
+      <VisibleMarkerList />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
